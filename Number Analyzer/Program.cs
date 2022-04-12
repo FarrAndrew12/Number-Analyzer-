bd841b0c2c3fa29205b2cf5bc524594f333c011c
FarrAndrew12 <101841142+FarrAndrew12@users.noreply.github.com>
2022-04-12 14:57:06 -0400

    First commit

---
+++ Number Analyzer/Program.cs
@@ -0,0 +1,79 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hey what is your name?");
+        Console.WriteLine();
+        string name = Console.ReadLine();
+        bool continueLoop = true;
+        int number = 0;
+
+        while (continueLoop)
+        {
+            while (true)  
+                //this will only continue to the next part of the loop if the number entered meets the requirement of the condition
+                //otherwise it will ask for a valid integer 
+            {
+                Console.WriteLine("Hello " + name + " please enter an integer between 1 and 100");
+                int enteredInteger = int.Parse(Console.ReadLine());
+
+                number = enteredInteger;
+
+                if (number >= 1 && number <= 100)
+                {
+                    Console.WriteLine("Okay " + name + " your number is " + number);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry " + name + " please try again");
+                }
+            }
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("The number you entered is even");
+                }
+                else
+                {
+                    Console.WriteLine("The number you entered is odd");
+                }
+                if (number % 2 != 0 && number < 60)
+                {
+                    Console.WriteLine("and less than 60");
+                }
+                else if (number % 2 == 0 && number < 25)
+                {
+                    Console.WriteLine("and less than 25");
+                }
+                else if (number % 2 == 0 && number <= 60)
+                {
+                    Console.WriteLine("and between 26 and 60 inclusive");
+                }
+                else if (number % 2 == 0 && number > 60)
+                {
+                    Console.WriteLine("and greater than 60");
+                }
+                else if (number % 2 != 0 && number > 60)
+                {
+                    Console.WriteLine("and greater than 60");
+                }
+                
+   
+            
+            Console.WriteLine(name + " would you like to run this again? Press y to continue:");
+            string userResponse = Console.ReadLine().ToLower();
+
+            if (userResponse == "y")
+            {
+                Console.WriteLine("Not a problem :)");
+     
+            }
+            else
+            {
+                Console.WriteLine("You are now exiting the program. Have a nice day!");
+                
+                break;
+            }
+        }
+    }
+}
